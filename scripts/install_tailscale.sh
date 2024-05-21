1a88c641aa6215fbf75cac0d34e50be61f67a30a
VascoRegal <vascoregal24@gmail.com>
2024-05-21 17:14:38 +0100

    client install script

---
+++ scripts/install_tailscale.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TRUSTSCALE_REPO="https://github.com/VascoRegal/trustscale"
+TRUSTSCALE_ZIP="trustscale.zip"
+TRUSTSCALE_ZIP_PATH="/raw/main/trustscale/${TRUSTSCALE_ZIP}"
+HEADSCALE_CERT="headscale.crt"
+HEADSCALE_CERT_URL="https://raw.githubusercontent.com/VascoRegal/ua-overlays-automation/main/certs/${HEADSCALE_CERT}"
+
+# Fetch zip with custom binaries
+wget ${TRUSTSCALE_REPO}${TRUSTSCALE_ZIP_PATH}
+unzip ${TRUSTSCALE_ZIP}
+
+# Install binaries
+cp tailscaled /etc/default/tailscaled
+cp tailscaled /usr/sbin/tailscaled
+cp tailscale /usr/bin/tailscale
+
+# Setup and enable tailscaled as a service
+cp tailscaled.service /lib/systemd/system/tailscaled.service
+service tailscaled enable
+service start tailscaled
+
+# Add Headscale certificate
+wget ${HEADSCALE_CERT_URL}
+cp ${HEADSCALE_CERT} /usr/local/share/ca-certificates/headscale.crt
+update-ca-certificates
+
+exit 0
