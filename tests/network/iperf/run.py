f7d3546b6ae8fee992a3b200eb96748aabd56131
VascoRegal <vascoregal24@gmail.com>
2024-05-22 02:39:22 +0100

    update testing script

--- tests/network/iperf/run.py
+++ tests/network/iperf/run.py
@@ -4,17 +4,19 @@ import subprocess
 import json
 import time
 
-TEST_DURATION=1
-MAX_PING_REPS=1
+TEST_DURATION=120
+MAX_PING_REPS=60
 
 def iperf_base(host):
-    return f"iperf3 -c {host} --json -t {TEST_DURATION}"
+    return f"iperf3 -c {host} --json -t {TEST_DURATION} "
 
-def test_iperf_tcp(host):
-    return run(iperf_base(host))
-
-def test_iperf_udp(host):
-    return run(iperf_base(host) + f"--udp ")
+def test_iperf(host, udp=False):
+    command = iperf_base(host)
+    if udp:
+        command += "-u"
+    results = run(command)
+    time.sleep(2)
+    return results
 
 def test_latency(host):
     return float(run(f"ping -c {MAX_PING_REPS} www.stackoverflow.com | tail -1| awk '{{print $4}}' | cut -d '/' -f 2", False))
@@ -30,12 +32,12 @@ def run_tests(host, building):
     
 
     print("\t[-] Running IPERF TCP tests...")
-    tcp = test_iperf_tcp(host)
+    tcp = test_iperf(host)
     print("\t[-] Done!")
     print()
 
     print("\t[-] Running IPERF UDP tests...")
-    udp = test_iperf_udp(host)
+    udp = test_iperf(host,udp=True)
     print("\t[-] Done!")
     print()
 
@@ -72,18 +74,19 @@ if __name__ == '__main__':
     iperf_host = sys.argv[1]
     building = sys.argv[2]
     day = datetime.today().strftime('%d-%m-%y')
-    time = datetime.now().time().strftime('%H-%M-%S')
+    hours = datetime.now().time().strftime('%H-%M-%S')
 
-    test_name = f"{building}_{day}_{time}"
+    test_name = f"{building}_{day}_{hours}"
 
     print(f"[+] Starting tests from {building}...")
     print(f"[+] Exporting runs to {test_name}.json")
 
     res = run_tests(iperf_host, building)
     res['date']['day'] = day
-    res['date']['time'] = time
+    res['date']['time'] = hours
 
     with open(f"runs/{test_name}.json", "w") as fp:
-        json.dump(res, fp)
+        json.dump(res, fp, indent=4)
+
 
 
