efa6a6bfc3afde7d9ddda0de4799aff1b5d4c6eb
VascoRegal <60277746+VascoRegal@users.noreply.github.com>
2024-09-11 08:47:05 +0100

    Update install_tailscale.sh

--- scripts/install_tailscale.sh
+++ scripts/install_tailscale.sh
@@ -3,11 +3,7 @@
 
 usage () {
 	printf "sudo sh ./install_tailscale.sh\n\n"
-	printf "\tThis script installs Tailscale using Trustscale binaries.\n"
-	printf "\tTrustscale is a Tailscale fork allowing the use of self-signed certificates.\n"
-	printf "\tThis script must be run as sudo.\n\n"
-	printf "\tThe script downloads a zip with binaries and configurations files and\n"
-	printf "\t\tinstalls them in the system.\n"
+	printf "\tThis script installs Tailscale using the official install script.\n"
        	printf "\tIt also fetches and setups Headscale certificates.\n\n"	
 }
 
@@ -17,25 +13,11 @@ if [ "$(id -u)" -ne 0 ]; then
 	exit 1
 fi
 
-SELFSCALE_REPO="https://github.com/VascoRegal/selfscale"
-SELFSCALE_ZIP="selfscale.zip"
-SELFSCALE_ZIP_PATH="/releases/download/beta/${SELFSCALE_ZIP}"
 HEADSCALE_CERT="headscale.crt"
 HEADSCALE_CERT_URL="https://raw.githubusercontent.com/VascoRegal/ua-overlays-automation/main/certs/${HEADSCALE_CERT}"
 
-# Fetch zip with custom binaries
-wget ${SELFSCALE_REPO}${SELFSCALE_ZIP_PATH}
-unzip ${SELFSCALE_ZIP}
-
-# Install binaries and configs
-cp tailscaled /usr/sbin/tailscaled
-cp tailscale /usr/bin/tailscale
-
-# Setup and enable tailscaled as a service
-cp tailscaled.service.conf /etc/default/tailscaled
-cp tailscaled.service /lib/systemd/system/tailscaled.service
-systemctl enable tailscaled
-systemctl start tailscaled
+# Install Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
 
 # Add Headscale certificate
 wget ${HEADSCALE_CERT_URL}
